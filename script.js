ae22f345e45816733bac605ce6a1befebfc5369f
aidanakmatova13 <akmatova.don@gmail.com>
2021-10-14 01:10:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+// const http = require('http') //в node.js модуль http создает бэкенд
+//
+// const url = 'localhost'
+// const port = 8000
+//
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/users'){
+//         res.write('give')
+//     }
+//     if (req.url === '/catalog'){
+//         res.write('отдам весь каталго')
+//     }
+//     res.end()
+// })
+//
+// server.listen(port, url, () =>{
+//     console.log('server is running')
+// })
+
+const express = require('express')
+const fs = require('fs')
+const cors = require('cors')
+
+const server = express()
+server.use(cors())
+server.use(express.json())
+
+
+const getAllUsers = () => JSON.parse(fs.readFileSync('users.json', 'utf8'))
+
+server.get('/api/users', (req, res) => {
+    const users = getAllUsers()
+    res.json(users)
+})
+server.get('/api/users/:id', (req, res) => {
+    const users = getAllUsers()
+    const selectedUser = users.find(el => el.id === +req.params.id)
+    if (selectedUser) {
+        res.json(selectedUser)
+    } else {
+        res.status(404).json({status: 'Not found'})
+    }
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const users = getAllUsers()
+    const deletedUser = users.find(el => el.id === +req.params.id)
+    const filteredUsers = users.filter(el => el.id !== +req.params.id)
+    fs.writeFileSync('users.json', JSON.stringify(filteredUsers, null, 2))
+    res.json(deletedUser)
+})
+server.put('/api/users/:id',(req,res) => {
+    const updatedUsers =  getAllUsers().map(el => el.id ===  +req.params.id ? {...el,...req.body} : el )
+    fs.writeFileSync('users.json',JSON.stringify(updatedUsers,null,2))
+    res.json(updatedUsers)
+})
+
+server.post('/api/users/:id',(req,res) => {
+    console.log(req.body)
+    const addUser =  [...getAllUsers(), req.body]
+    fs.writeFileSync('users.json',JSON.stringify(addUser,null,2))
+    res.json(addUser)
+})
+
+// server.post('/api/users/:id', (req, res) => {
+//     const users = getAllUsers()
+// })
+
+server.listen(8000, () => {
+    console.log('Server is running')
+})
